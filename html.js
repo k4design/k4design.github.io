e227077f2ede1a7d71b7d53b111107c40616e636
Kyle Foreman <kyleforeman.design@gmail.com>
2023-10-15 18:18:54 -0400

    Create html.js

---
+++ html.js
@@ -0,0 +1,6 @@
+
+        function addCode() {
+            document.getElementById("add_to_me")
+                .innerHTML +=
+"<h3>This is the text which has been inserted by JS</h3>";
+        }
