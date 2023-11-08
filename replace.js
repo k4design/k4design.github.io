497b6b9a977e507774bdbf728e11ff1843a49373
Kyle Foreman <kyleforeman.design@gmail.com>
2023-11-08 15:48:59 -0500

    stuff

---
+++ replace.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // This code runs after the page has finished loading
+
+    // Trigger the content replacement here (e.g., after a delay)
+    setTimeout(replaceContent, 2000); // Replace content after 2 seconds
+
+    function replaceContent() {
+        var url = 'https://k4design.github.io/message.html';
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var externalHTML = xhr.responseText;
+                var elementToReplace = document.querySelector('.md-hero.template4 .media-content .media-info.md-hero.template4 .media-content .media-info');
+                elementToReplace.innerHTML = externalHTML;
+            }
+        };
+
+        xhr.send();
+    }
+});
